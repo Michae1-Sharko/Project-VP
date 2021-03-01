37b3ed52e4186e3818779ba5c383e34d87da0704
Michael Sharko <michael.sharko@yandex.ru>
2021-03-01 09:11:18 +0300

    Добавлена группа камер для наблюдения (элементы panel и pictureBox) за котом. Также описаны их начальные свойства

--- Project-VP/Form1.Designer.cs
+++ Project-VP/Form1.Designer.cs
@@ -29,13 +29,258 @@ namespace Project_VP
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.panelBackgroundUp = new System.Windows.Forms.Panel();
+            this.panelCatUp = new System.Windows.Forms.Panel();
+            this.panelBackgroundDown = new System.Windows.Forms.Panel();
+            this.panelCatDown = new System.Windows.Forms.Panel();
+            this.panelBackgroundLeft = new System.Windows.Forms.Panel();
+            this.panelCatLeft = new System.Windows.Forms.Panel();
+            this.panelBackgroundRight = new System.Windows.Forms.Panel();
+            this.panelCatRight = new System.Windows.Forms.Panel();
+            this.pictureCatRight = new System.Windows.Forms.PictureBox();
+            this.pictureBackgroundRight = new System.Windows.Forms.PictureBox();
+            this.pictureCatLeft = new System.Windows.Forms.PictureBox();
+            this.pictureBackgroundLeft = new System.Windows.Forms.PictureBox();
+            this.pictureCatDown = new System.Windows.Forms.PictureBox();
+            this.pictureBackgroundDown = new System.Windows.Forms.PictureBox();
+            this.pictureCatUp = new System.Windows.Forms.PictureBox();
+            this.pictureBackgroundUp = new System.Windows.Forms.PictureBox();
+            this.panelBackgroundUp.SuspendLayout();
+            this.panelCatUp.SuspendLayout();
+            this.panelBackgroundDown.SuspendLayout();
+            this.panelCatDown.SuspendLayout();
+            this.panelBackgroundLeft.SuspendLayout();
+            this.panelCatLeft.SuspendLayout();
+            this.panelBackgroundRight.SuspendLayout();
+            this.panelCatRight.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureCatRight)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBackgroundRight)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureCatLeft)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBackgroundLeft)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureCatDown)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBackgroundDown)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureCatUp)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBackgroundUp)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panelBackgroundUp
+            // 
+            this.panelBackgroundUp.Controls.Add(this.panelCatUp);
+            this.panelBackgroundUp.Controls.Add(this.pictureBackgroundUp);
+            this.panelBackgroundUp.Location = new System.Drawing.Point(12, 12);
+            this.panelBackgroundUp.Margin = new System.Windows.Forms.Padding(3, 3, 0, 0);
+            this.panelBackgroundUp.Name = "panelBackgroundUp";
+            this.panelBackgroundUp.Size = new System.Drawing.Size(160, 160);
+            this.panelBackgroundUp.TabIndex = 0;
+            // 
+            // panelCatUp
+            // 
+            this.panelCatUp.BackColor = System.Drawing.Color.Transparent;
+            this.panelCatUp.Controls.Add(this.pictureCatUp);
+            this.panelCatUp.Location = new System.Drawing.Point(64, 64);
+            this.panelCatUp.Name = "panelCatUp";
+            this.panelCatUp.Size = new System.Drawing.Size(32, 32);
+            this.panelCatUp.TabIndex = 0;
+            // 
+            // panelBackgroundDown
+            // 
+            this.panelBackgroundDown.Controls.Add(this.panelCatDown);
+            this.panelBackgroundDown.Controls.Add(this.pictureBackgroundDown);
+            this.panelBackgroundDown.Location = new System.Drawing.Point(175, 12);
+            this.panelBackgroundDown.Margin = new System.Windows.Forms.Padding(3, 3, 0, 0);
+            this.panelBackgroundDown.Name = "panelBackgroundDown";
+            this.panelBackgroundDown.Size = new System.Drawing.Size(160, 160);
+            this.panelBackgroundDown.TabIndex = 1;
+            // 
+            // panelCatDown
+            // 
+            this.panelCatDown.Controls.Add(this.pictureCatDown);
+            this.panelCatDown.Location = new System.Drawing.Point(64, 64);
+            this.panelCatDown.Name = "panelCatDown";
+            this.panelCatDown.Size = new System.Drawing.Size(32, 32);
+            this.panelCatDown.TabIndex = 0;
+            // 
+            // panelBackgroundLeft
+            // 
+            this.panelBackgroundLeft.Controls.Add(this.panelCatLeft);
+            this.panelBackgroundLeft.Controls.Add(this.pictureBackgroundLeft);
+            this.panelBackgroundLeft.Location = new System.Drawing.Point(12, 175);
+            this.panelBackgroundLeft.Margin = new System.Windows.Forms.Padding(3, 3, 0, 0);
+            this.panelBackgroundLeft.Name = "panelBackgroundLeft";
+            this.panelBackgroundLeft.Size = new System.Drawing.Size(160, 160);
+            this.panelBackgroundLeft.TabIndex = 1;
+            // 
+            // panelCatLeft
+            // 
+            this.panelCatLeft.Controls.Add(this.pictureCatLeft);
+            this.panelCatLeft.Location = new System.Drawing.Point(64, 64);
+            this.panelCatLeft.Name = "panelCatLeft";
+            this.panelCatLeft.Size = new System.Drawing.Size(32, 32);
+            this.panelCatLeft.TabIndex = 0;
+            // 
+            // panelBackgroundRight
+            // 
+            this.panelBackgroundRight.Controls.Add(this.panelCatRight);
+            this.panelBackgroundRight.Controls.Add(this.pictureBackgroundRight);
+            this.panelBackgroundRight.Location = new System.Drawing.Point(175, 175);
+            this.panelBackgroundRight.Margin = new System.Windows.Forms.Padding(3, 3, 0, 0);
+            this.panelBackgroundRight.Name = "panelBackgroundRight";
+            this.panelBackgroundRight.Size = new System.Drawing.Size(160, 160);
+            this.panelBackgroundRight.TabIndex = 1;
+            // 
+            // panelCatRight
+            // 
+            this.panelCatRight.Controls.Add(this.pictureCatRight);
+            this.panelCatRight.Location = new System.Drawing.Point(64, 64);
+            this.panelCatRight.Name = "panelCatRight";
+            this.panelCatRight.Size = new System.Drawing.Size(32, 32);
+            this.panelCatRight.TabIndex = 0;
+            // 
+            // pictureCatRight
+            // 
+            this.pictureCatRight.Image = global::Project_VP.Properties.Resources.cat;
+            this.pictureCatRight.Location = new System.Drawing.Point(0, -64);
+            this.pictureCatRight.Margin = new System.Windows.Forms.Padding(0);
+            this.pictureCatRight.Name = "pictureCatRight";
+            this.pictureCatRight.Size = new System.Drawing.Size(384, 256);
+            this.pictureCatRight.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureCatRight.TabIndex = 0;
+            this.pictureCatRight.TabStop = false;
+            // 
+            // pictureBackgroundRight
+            // 
+            this.pictureBackgroundRight.Image = global::Project_VP.Properties.Resources.roomRight;
+            this.pictureBackgroundRight.Location = new System.Drawing.Point(64, -128);
+            this.pictureBackgroundRight.Margin = new System.Windows.Forms.Padding(0);
+            this.pictureBackgroundRight.Name = "pictureBackgroundRight";
+            this.pictureBackgroundRight.Size = new System.Drawing.Size(320, 416);
+            this.pictureBackgroundRight.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBackgroundRight.TabIndex = 0;
+            this.pictureBackgroundRight.TabStop = false;
+            // 
+            // pictureCatLeft
+            // 
+            this.pictureCatLeft.Image = global::Project_VP.Properties.Resources.cat;
+            this.pictureCatLeft.Location = new System.Drawing.Point(0, -32);
+            this.pictureCatLeft.Margin = new System.Windows.Forms.Padding(0);
+            this.pictureCatLeft.Name = "pictureCatLeft";
+            this.pictureCatLeft.Size = new System.Drawing.Size(384, 256);
+            this.pictureCatLeft.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureCatLeft.TabIndex = 0;
+            this.pictureCatLeft.TabStop = false;
+            // 
+            // pictureBackgroundLeft
+            // 
+            this.pictureBackgroundLeft.Image = global::Project_VP.Properties.Resources.roomLeft;
+            this.pictureBackgroundLeft.Location = new System.Drawing.Point(-96, -128);
+            this.pictureBackgroundLeft.Margin = new System.Windows.Forms.Padding(0);
+            this.pictureBackgroundLeft.Name = "pictureBackgroundLeft";
+            this.pictureBackgroundLeft.Size = new System.Drawing.Size(320, 416);
+            this.pictureBackgroundLeft.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBackgroundLeft.TabIndex = 0;
+            this.pictureBackgroundLeft.TabStop = false;
+            // 
+            // pictureCatDown
+            // 
+            this.pictureCatDown.Image = global::Project_VP.Properties.Resources.cat;
+            this.pictureCatDown.Location = new System.Drawing.Point(0, -96);
+            this.pictureCatDown.Margin = new System.Windows.Forms.Padding(0);
+            this.pictureCatDown.Name = "pictureCatDown";
+            this.pictureCatDown.Size = new System.Drawing.Size(384, 256);
+            this.pictureCatDown.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureCatDown.TabIndex = 0;
+            this.pictureCatDown.TabStop = false;
+            // 
+            // pictureBackgroundDown
+            // 
+            this.pictureBackgroundDown.Image = global::Project_VP.Properties.Resources.roomDown;
+            this.pictureBackgroundDown.Location = new System.Drawing.Point(-128, -160);
+            this.pictureBackgroundDown.Margin = new System.Windows.Forms.Padding(0);
+            this.pictureBackgroundDown.Name = "pictureBackgroundDown";
+            this.pictureBackgroundDown.Size = new System.Drawing.Size(416, 320);
+            this.pictureBackgroundDown.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBackgroundDown.TabIndex = 0;
+            this.pictureBackgroundDown.TabStop = false;
+            // 
+            // pictureCatUp
+            // 
+            this.pictureCatUp.Image = global::Project_VP.Properties.Resources.cat;
+            this.pictureCatUp.Location = new System.Drawing.Point(0, 0);
+            this.pictureCatUp.Margin = new System.Windows.Forms.Padding(0);
+            this.pictureCatUp.Name = "pictureCatUp";
+            this.pictureCatUp.Size = new System.Drawing.Size(384, 256);
+            this.pictureCatUp.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureCatUp.TabIndex = 0;
+            this.pictureCatUp.TabStop = false;
+            // 
+            // pictureBackgroundUp
+            // 
+            this.pictureBackgroundUp.Image = global::Project_VP.Properties.Resources.roomUp;
+            this.pictureBackgroundUp.Location = new System.Drawing.Point(-128, 0);
+            this.pictureBackgroundUp.Margin = new System.Windows.Forms.Padding(0);
+            this.pictureBackgroundUp.Name = "pictureBackgroundUp";
+            this.pictureBackgroundUp.Size = new System.Drawing.Size(416, 320);
+            this.pictureBackgroundUp.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBackgroundUp.TabIndex = 0;
+            this.pictureBackgroundUp.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panelBackgroundRight);
+            this.Controls.Add(this.panelBackgroundLeft);
+            this.Controls.Add(this.panelBackgroundDown);
+            this.Controls.Add(this.panelBackgroundUp);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.panelBackgroundUp.ResumeLayout(false);
+            this.panelBackgroundUp.PerformLayout();
+            this.panelCatUp.ResumeLayout(false);
+            this.panelCatUp.PerformLayout();
+            this.panelBackgroundDown.ResumeLayout(false);
+            this.panelBackgroundDown.PerformLayout();
+            this.panelCatDown.ResumeLayout(false);
+            this.panelCatDown.PerformLayout();
+            this.panelBackgroundLeft.ResumeLayout(false);
+            this.panelBackgroundLeft.PerformLayout();
+            this.panelCatLeft.ResumeLayout(false);
+            this.panelCatLeft.PerformLayout();
+            this.panelBackgroundRight.ResumeLayout(false);
+            this.panelBackgroundRight.PerformLayout();
+            this.panelCatRight.ResumeLayout(false);
+            this.panelCatRight.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureCatRight)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBackgroundRight)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureCatLeft)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBackgroundLeft)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureCatDown)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBackgroundDown)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureCatUp)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBackgroundUp)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panelBackgroundUp;
+        private System.Windows.Forms.Panel panelCatUp;
+        private System.Windows.Forms.PictureBox pictureCatUp;
+        private System.Windows.Forms.PictureBox pictureBackgroundUp;
+        private System.Windows.Forms.Panel panelBackgroundDown;
+        private System.Windows.Forms.Panel panelCatDown;
+        private System.Windows.Forms.PictureBox pictureCatDown;
+        private System.Windows.Forms.PictureBox pictureBackgroundDown;
+        private System.Windows.Forms.Panel panelBackgroundLeft;
+        private System.Windows.Forms.Panel panelCatLeft;
+        private System.Windows.Forms.PictureBox pictureCatLeft;
+        private System.Windows.Forms.PictureBox pictureBackgroundLeft;
+        private System.Windows.Forms.Panel panelBackgroundRight;
+        private System.Windows.Forms.Panel panelCatRight;
+        private System.Windows.Forms.PictureBox pictureCatRight;
+        private System.Windows.Forms.PictureBox pictureBackgroundRight;
     }
 }
 
